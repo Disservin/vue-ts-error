f44a7b4fca3e5ce9282cc83ada1360373a881be4
disservin <disservin.social@gmail.com>
2023-06-16 16:32:03 +0200

    Add files via upload

---
+++ vue-ts-error/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import VTest from "@/components/VTest.vue";
+</script>
+
+<template>
+    <main>
+        <v-test email="test" name="test"></v-test>
+    </main>
+</template>
+
+<style scoped></style>
---
+++ vue-ts-error/src/components/VTest.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Test</div>
+</template>
+
+<script setup lang="ts">
+interface Extended extends Test {
+    email: string;
+}
+
+defineProps<Extended>();
+</script>
+
+<style scoped></style>
